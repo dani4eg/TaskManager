b021a95e7a5dad0f25b1ddaed53b57d06078467d
darthMilash <dani4eg92@gmail.com>
2017-02-17 13:45:55 +0200

    add Interval class

--- src/main/java/model/TaskIO.java
+++ src/main/java/model/TaskIO.java
@@ -118,7 +118,7 @@ public class TaskIO {
         try(BufferedReader reader = new BufferedReader(in)) {
             String str;
             while((str=reader.readLine())!=null){
-                tasks.add(splitString(str));
+                tasks.add(Interval.splitString(str));
             }
             logger.info("Text reading task SUCCESSFUL");
         }catch(IOException e){
@@ -154,127 +154,127 @@ public class TaskIO {
         }
     }
 
-    public static Task splitString(String str) throws ParseException {
-        Task task;
-        String title = "";
-        Date dStart = null;
-        Date dEnd = null;
-        Date dTime = null;
-        int interval=0;
-        boolean active = false;
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss.SSS", Locale.ENGLISH);
-
-        /**
-         * Title
-         */
-        Pattern patTitle = Pattern.compile("\".+\"");
-        Matcher mTitle = patTitle.matcher(str);
-        if (mTitle.find()) {
-            title = str.substring(mTitle.start()+1, mTitle.end()-1);;
-        }
-
-        /**
-         * DateStart
-         */
-        String start = "";
-        Pattern patStart = Pattern.compile("from.+to");
-        Matcher mStart = patStart.matcher(str);
-        if (mStart.find()) {
-            start = str.substring(mStart.start()+6, mStart.end()-4);
-            dStart=date.parse(start);
-        }
-
-        /**
-         * DateEnd
-         */
-        String end = "";
-        Pattern patEnd = Pattern.compile("to.+every");
-        Matcher mEnd = patEnd.matcher(str);
-        if (mEnd.find()) {
-            end = str.substring(mEnd.start()+4, mEnd.end()-7);
-            dEnd=date.parse(end);
-        }
-
-        /**
-         * DateTime
-         */
-        String time = "";
-        Pattern patTime = Pattern.compile("at.+]");
-        Matcher mTime = patTime.matcher(str);
-        if (mTime.find()) {
-            time = str.substring(mTime.start()+4, mTime.end()-1);
-            dTime = date.parse(time);
-        }
-
-        /**
-         * Interval
-         */
-        String days = "";
-        String hours = "";
-        String minutes = "";
-        String seconds = "";
-        Pattern pat = Pattern.compile("every.+day");
-        Matcher matcher = pat.matcher(str);
-        if (matcher.find()) {
-            days = str.substring(matcher.start()+7, matcher.end()-4);
-            interval=Integer.parseInt(days) * 86400;
-        }
-        Pattern pat2 = Pattern.compile("hour");
-        Matcher matcher2 = pat2.matcher(str);
-        if (matcher2.find()) {
-            hours = str.substring(matcher2.start()-3, matcher2.end()-5);
-            try {
-                interval+= Integer.parseInt(hours) * 3600;
-            }
-            catch (NumberFormatException e) {
-                String cHours = str.substring(matcher2.start()-2, matcher2.end()-5);
-                interval+= Integer.parseInt(cHours) * 3600;
-            }
-        }
-        Pattern pat3 = Pattern.compile("minute");
-        Matcher matcher3 = pat3.matcher(str);
-        if (matcher3.find()) {
-            minutes = str.substring(matcher3.start()-3, matcher3.end()-7);
-            try {
-                interval+= Integer.parseInt(minutes) * 60;
-            }
-            catch (NumberFormatException e) {
-                String cMinutes = str.substring(matcher3.start()-2, matcher3.end()-7);
-                interval+= Integer.parseInt(cMinutes) * 60;
-            }
-        }
-        Pattern patSec = Pattern.compile("second");
-        Matcher mSec = patSec.matcher(str);
-        if (mSec.find()) {
-            seconds = str.substring(mSec.start()-3, mSec.end()-7);
-            try {
-                interval+= Integer.parseInt(seconds);
-            }
-            catch (NumberFormatException e) {
-                String cSeconds = str.substring(mSec.start()-2, mSec.end()-7);
-                interval+= Integer.parseInt(cSeconds);
-            }
-        }
-
-        /**
-         * Active
-         */
-        Pattern patActive = Pattern.compile("inactive");
-        Matcher mActive = patActive.matcher(str);
-        if (mActive.find()) {
-            active = true;
-        }
-
-        /**
-         * Create task
-         */
-        if (dStart==null && dEnd==null) {
-            task = new Task(title, dTime);
-        }
-        else task = new Task(title, dStart, dEnd, interval);
-        if (active) {
-            task.setActive(true);
-        }
-        return task;
-    }
+//    public static Task splitString(String str) throws ParseException {
+//        Task task;
+//        String title = "";
+//        Date dStart = null;
+//        Date dEnd = null;
+//        Date dTime = null;
+//        int interval=0;
+//        boolean active = false;
+//        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss.SSS", Locale.ENGLISH);
+//
+//        /**
+//         * Title
+//         */
+//        Pattern patTitle = Pattern.compile("\".+\"");
+//        Matcher mTitle = patTitle.matcher(str);
+//        if (mTitle.find()) {
+//            title = str.substring(mTitle.start()+1, mTitle.end()-1);;
+//        }
+//
+//        /**
+//         * DateStart
+//         */
+//        String start = "";
+//        Pattern patStart = Pattern.compile("from.+to");
+//        Matcher mStart = patStart.matcher(str);
+//        if (mStart.find()) {
+//            start = str.substring(mStart.start()+6, mStart.end()-4);
+//            dStart=date.parse(start);
+//        }
+//
+//        /**
+//         * DateEnd
+//         */
+//        String end = "";
+//        Pattern patEnd = Pattern.compile("to.+every");
+//        Matcher mEnd = patEnd.matcher(str);
+//        if (mEnd.find()) {
+//            end = str.substring(mEnd.start()+4, mEnd.end()-7);
+//            dEnd=date.parse(end);
+//        }
+//
+//        /**
+//         * DateTime
+//         */
+//        String time = "";
+//        Pattern patTime = Pattern.compile("at.+]");
+//        Matcher mTime = patTime.matcher(str);
+//        if (mTime.find()) {
+//            time = str.substring(mTime.start()+4, mTime.end()-1);
+//            dTime = date.parse(time);
+//        }
+//
+//        /**
+//         * Interval
+//         */
+//        String days = "";
+//        String hours = "";
+//        String minutes = "";
+//        String seconds = "";
+//        Pattern pat = Pattern.compile("every.+day");
+//        Matcher matcher = pat.matcher(str);
+//        if (matcher.find()) {
+//            days = str.substring(matcher.start()+7, matcher.end()-4);
+//            interval=Integer.parseInt(days) * 86400;
+//        }
+//        Pattern pat2 = Pattern.compile("hour");
+//        Matcher matcher2 = pat2.matcher(str);
+//        if (matcher2.find()) {
+//            hours = str.substring(matcher2.start()-3, matcher2.end()-5);
+//            try {
+//                interval+= Integer.parseInt(hours) * 3600;
+//            }
+//            catch (NumberFormatException e) {
+//                String cHours = str.substring(matcher2.start()-2, matcher2.end()-5);
+//                interval+= Integer.parseInt(cHours) * 3600;
+//            }
+//        }
+//        Pattern pat3 = Pattern.compile("minute");
+//        Matcher matcher3 = pat3.matcher(str);
+//        if (matcher3.find()) {
+//            minutes = str.substring(matcher3.start()-3, matcher3.end()-7);
+//            try {
+//                interval+= Integer.parseInt(minutes) * 60;
+//            }
+//            catch (NumberFormatException e) {
+//                String cMinutes = str.substring(matcher3.start()-2, matcher3.end()-7);
+//                interval+= Integer.parseInt(cMinutes) * 60;
+//            }
+//        }
+//        Pattern patSec = Pattern.compile("second");
+//        Matcher mSec = patSec.matcher(str);
+//        if (mSec.find()) {
+//            seconds = str.substring(mSec.start()-3, mSec.end()-7);
+//            try {
+//                interval+= Integer.parseInt(seconds);
+//            }
+//            catch (NumberFormatException e) {
+//                String cSeconds = str.substring(mSec.start()-2, mSec.end()-7);
+//                interval+= Integer.parseInt(cSeconds);
+//            }
+//        }
+//
+//        /**
+//         * Active
+//         */
+//        Pattern patActive = Pattern.compile("inactive");
+//        Matcher mActive = patActive.matcher(str);
+//        if (mActive.find()) {
+//            active = true;
+//        }
+//
+//        /**
+//         * Create task
+//         */
+//        if (dStart==null && dEnd==null) {
+//            task = new Task(title, dTime);
+//        }
+//        else task = new Task(title, dStart, dEnd, interval);
+//        if (active) {
+//            task.setActive(true);
+//        }
+//        return task;
+//    }
 }
